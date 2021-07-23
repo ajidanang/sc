4ac5ebc5dc4c4e1a75757a7fca161a31665e4e70
lesta-1 <82885098+lesta-1@users.noreply.github.com>
2021-07-23 21:47:34 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -312,10 +312,10 @@ wget -O port-vless "https://raw.githubusercontent.com/lesta-1/sc/main/port-vless
 wget -O wbmn "https://raw.githubusercontent.com/lesta-1/sc/main/webmin.sh"
 wget -O xp "https://raw.githubusercontent.com/lesta-1/sc/main/xp.sh"
 wget -O update "https://raw.githubusercontent.com/lesta-1/sc/main/update.sh"
-wget -O add-cfh "https://raw.githubusercontent.com/lesta-1/sc/main/cfh.sh"
-wget -O add-cfd "https://raw.githubusercontent.com/lesta-1/sc/main/cfd.sh"
-wget -O add-cff "https://raw.githubusercontent.com/lesta-1/sc/main/cff.sh"
-wget -O add-cff "https://raw.githubusercontent.com/lesta-1/sc/main/running.sh"
+wget -O cfh "https://raw.githubusercontent.com/lesta-1/sc/main/cfh.sh"
+wget -O cfd "https://raw.githubusercontent.com/lesta-1/sc/main/cfd.sh"
+wget -O cff "https://raw.githubusercontent.com/lesta-1/sc/main/cff.sh"
+wget -O cff "https://raw.githubusercontent.com/lesta-1/sc/main/running.sh"
 chmod +x running
 chmod +x cfh
 chmod +x cfd
