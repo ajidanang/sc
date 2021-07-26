b424a361f6f7264ae7572dde607274c5c0e402b3
lesta-1 <82885098+lesta-1@users.noreply.github.com>
2021-07-26 17:40:22 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -218,7 +218,7 @@ sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
 apt -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 109"/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 109"-p 69"/g' /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
 /etc/init.d/dropbear restart
@@ -256,8 +256,8 @@ socket = l:TCP_NODELAY=1
 socket = r:TCP_NODELAY=1
 
 [dropbear]
-accept = 443
-connect = 127.0.0.1:109
+accept = 445
+connect = 127.0.0.1:22
 
 [dropbear]
 accept = 777
@@ -266,6 +266,10 @@ connect = 127.0.0.1:22
 [openvpn]
 accept = 442
 connect = 127.0.0.1:1194
+
+[wsssl]
+accept = 443
+connect = 700
 
 END
 
