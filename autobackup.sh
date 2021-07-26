14ea51de5e220da5d0263d4574b8fe67968e8418
lesta-1 <82885098+lesta-1@users.noreply.github.com>
2021-07-26 13:36:38 +0700

    Update autobackup.sh

--- autobackup.sh
+++ autobackup.sh
@@ -2,7 +2,7 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- ifconfig.me/ip);
+MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 IZIN=$( curl http://akses.rpj08.my.id:81/akses | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
