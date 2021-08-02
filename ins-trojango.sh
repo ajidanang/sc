289b13e77584fd9a4f6ed8f411efa689453a4761
lesta-1 <82885098+lesta-1@users.noreply.github.com>
2021-08-02 19:46:54 +0700

    Update ins-trojango.sh

--- ins-trojango.sh
+++ ins-trojango.sh
@@ -12,11 +12,12 @@ apt install zip
 domain=$(cat /etc/v2ray/domain)
 
 # Uuid Service
-uuid=$(cat /proc/sys/kernel/random/uuid)
+uuid=$(cat /etc/trojan-go/uuid.txt)
 
 # Trojan Go Akun 
 mkdir -p /etc/trojan-go/
 touch /etc/trojan-go/akun.conf
+touch /etc/trojan-go/uuid.txt
 
 # Installing Trojan Go
 mkdir -p /etc/trojan-go/
@@ -27,39 +28,19 @@ wget -O /etc/trojan-go/geoip.dat https://raw.githubusercontent.com/lesta-1/sc/ma
 wget -O /etc/trojan-go/geosite.dat https://raw.githubusercontent.com/lesta-1/sc/main/geosite.dat
 chmod +x /etc/trojan-go/trojan-go
 
-# Service
-cat > /etc/systemd/system/trojan-go.service << END
-[Unit]
-Description=Trojan-Go 
-Documentation=https://p4gefau1t.github.io/trojan-go/
-After=network.target nss-lookup.target
-
-[Service]
-User=root
-NoNewPrivileges=true
-ExecStart=/etc/trojan-go/trojan-go -config /etc/trojan-go/config.json
-Restart=on-failure
-RestartSec=10s
-LimitNOFILE=infinity
-
-[Install]
-WantedBy=multi-user.target
-END
-
-# Config
-cat > /etc/trojan-go/config.json << END
+cat <<EOF > /etc/trojan-go/config.json
 {
-  "run_type": "server",
-  "local_addr": "0.0.0.0",
-  "local_port": 443,
-  "remote_addr": "127.0.0.1",
-  "remote_port": 81,
-  "log_level": 1,
-  "log_file": "/var/log/trojan-go.log",
-  "password": [
-    "RPJ_WISANG"
-  ],
-  "disable_http_check": true,
+    "run_type": "server",
+    "local_addr": "0.0.0.0",
+    "local_port": 443,
+    "remote_addr": "127.0.0.1",
+    "remote_port": 81,
+    "log_level": 1,
+    "log_file": "/var/log/trojan-go.log",
+    "password": [
+        "$uuid"
+    ],
+  "disable_http_check": false,
   "udp_timeout": 60,
   "ssl": {
     "verify": true,
@@ -69,16 +50,16 @@ cat > /etc/trojan-go/config.json << END
     "key_password": "",
     "cipher": "",
     "curves": "",
-    "prefer_server_cipher": true,
+    "prefer_server_cipher": false,
     "sni": "$domain",
     "alpn": [
-      "http/1.1"
+      "h2"
     ],
     "session_ticket": true,
     "reuse_session": true,
     "plain_http_response": "",
     "fallback_addr": "127.0.0.1",
-    "fallback_port": 2095,
+    "fallback_port": 443,
     "fingerprint": ""
   },
   "tcp": {
@@ -98,12 +79,12 @@ cat > /etc/trojan-go/config.json << END
     "block": [],
     "default_policy": "proxy",
     "domain_strategy": "as_is",
-    "geoip": "/usr/local/bin/geoip.dat",
-    "geosite": "/usr/local/bin/geosite.dat"
+    "geoip": "/etc/trojan-go/geoip.dat",
+    "geosite": "/etc/trojan-go/geosite.dat"
   },
   "websocket": {
     "enabled": true,
-    "path": "/RPJ_WISANG",
+    "path": "/Endka",
     "host": "$domain"
   },
   "shadowsocks": {
@@ -148,11 +129,33 @@ cat > /etc/trojan-go/config.json << END
     }
   }
 }
-END
+EOF
+cat <<EOF > /etc/systemd/system/trojan-go.service
+[Unit]
+Description=Trojan-Go 
+Documentation=https://p4gefau1t.github.io/trojan-go/
+After=network.target nss-lookup.target
+
+[Service]
+User=root
+NoNewPrivileges=true
+ExecStart=/etc/trojan-go/trojan-go -config /etc/trojan-go/config.json
+Restart=on-failure
+RestartSec=10s
+LimitNOFILE=infinity
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+cat <<EOF > /etc/trojan/uuid.txt
+$uuid
+EOF
 iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
 iptables -I INPUT -m state --state NEW -m udp -p udp --dport 443 -j ACCEPT
-ip6tables -I INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
-ip6tables -I INPUT -m state --state NEW -m udp -p udp --dport 443 -j ACCEPT
+ip6tables -I INPUT -m state --state NEW -m tcp -p tcp --dport 81 -j ACCEPT
+ip6tables -I INPUT -m state --state NEW -m udp -p udp --dport 81 -j ACCEPT
 iptables-save >/etc/iptables.rules.v4
 ip6tables-save >/etc/iptables.rules.v6
 netfilter-persistent save
@@ -163,3 +166,5 @@ systemctl daemon-reload
 systemctl daemon-reload
 systemctl enable trojan-go.service
 systemctl start trojan-go
+rm -f ins-trojango.sh
+mv /root/domain /etc/v2ray
